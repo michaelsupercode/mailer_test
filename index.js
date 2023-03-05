7f0ef3a057cd18086020087775983b3f861ea0de
Michael <michaelsupercode@gmail.com>
2023-03-05 17:10:18 +0100

    basic set

---
+++ index.js
@@ -0,0 +1,17 @@
+var nodemailer = require('nodemailer');
+
+var connection = 'smtps://';
+
+var transporter = nodemailer.createTransport(connection);
+
+var mailOptions = {
+    from: '<>',
+    to: '',
+    subject: '',
+    text: ''
+};
+
+transporter.sendMail(mailOptions, function(err, info) {
+    if (err) throw err;
+    console.log('Message sent: ' + info.response);
+});
